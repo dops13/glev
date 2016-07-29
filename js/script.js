d14748ee9e096c00c9377f013636256c4de7e8a0
beshley <beshleyweb@yandex.ru>
2016-07-29 23:33:53 +0300

    29.07

--- js/script.js
+++ js/script.js
@@ -29,6 +29,24 @@ $(function() {
 	
 	/*popup blog*/
 	$('.news_item a').click(function(){
+		/*test data*/
+		var blog_data = '{"items": ['+
+		'{"title" : "Отримай своє запрошення на свято та виграй приз!", "content": "<p>Дорогі друзі, власники квартир у Галицькому Леві! Свято наближається, вже зараз Ви можете завітати до Центру продажів ЖК Галицький Лев та отримати офіційне запрошення на свято. Це запрошення дає можливість Вам виграти цінні призи від нашого ЖК у лототроні. Призи дійсно варті уваги! Поспішайте отримати запрошення!!! Перші запрошення вже видані!</p>" },' +
+		'{"title" : "Шановні мешканці 12 секції!", "content": "<p>Вітаємо Вас із газифікацією Вашого будинку! Відтепер Ви можете звертатись до відділу продажів з питання підписання актів прийому-передачі, оформлення техпаспортів та, на перспективу, отримання документів на право власності Ваших помешкань. З метою впорядкування черги та економії Вашого часу рекомендуємо попередньо зателефонувати до наших менеджерів. При собі мати паспорт, індентифікаційний код та Ваші договори!</p>" },' +
+		'{"title" : "Вивезення побутового сміття!", "content": "<p>Вивезення побутового сміття!  В проході 12-ї секції встановлено два баки для побутових відходів.</p><p>ПРОХАННЯ! Не викидувати до цих баків будівельних відходів!  По мірі заселення кварталу баки для ТПВ будуть доставлятися!!</p>" },' +
+		'{"title" : "Шановні мешканці 1,2,3-ї та 6-ї секцій! ", "content": "<p>У звязку із постійним ігноруванням наших прохань, щодо вивозу сміття з під Ваших секцій, не дотриманням належного санітарного стану секцій та місць загального користування, не сплатою за спожиті енергоносії — повідомляємо Вас про відключення електропостачання Ваших секцій до вирішення цих питань по суті.</p><p>Дякуємо за увагу!</p><p>Адміністрація ГЛ</p>" },' +
+		'{"title" : "Оформлення права власності", "content": "<p>Шановні мешканці 9ї та 10ї секцій, а також 6ї та 11ї, хто не встиг подати документи на оформлення права власності на Ваші квартири! Просимо Вас наступного тижня до 18 червня завітати з відповідним пакетом документів до Центру Продажів Галицького Лева. Оскільки через тиждень розпочнемо прийом документів інших секцій і Вам, доведеться чекати!</p><p>При собі мати: Паспорт власника, індентифікаційний код, акт прийому-передачі, довідку про повну сплату пайових внесків, договір та кошти для сплати реєстраційних послуг нотаріусу - 1400 грн. та 300 грн. за вироблення технічного паспорту (якщо не сплачували раніше).</p>" },' +
+		'{"title" : "Кубок Галицького Лева", "content": "<p>Шановні клієнти Галицького Лева, працівники компанії, - щиро запрошуємо Вас долучитись до футбольного матчу на кубок Галицького Лева.  Формуйте команди в складі 5-6 осіб (4-ро в полі, 1-2 - в запасі) та вперед за кубком! Дівчата - вболівають! Матч відбудеться на нашому футбольному майданчику у п\'ятницю,</p><p>10 червня 2016 року, о 11-00 годині. Заявки приймаються в коментарях до цього посту до кінця четверга - 9 червня.  Просимо написати прізвища, ім\'я гравців, телефон капітана команди та інформацію про гравців і команду. Назва команди, категорію гравців - наприклад, мешканці 10 секції, чи команда ІТРівців і т.д.</p>" },' +
+		'{"title" : "Дев\'ята секція! ", "content": "<p>Розпочинаємо процес приймання документів на оформлення прав власності. Поки для 1-3-го поерхів, зателефонуйте до Центру продажів та запишіться на прийом до менеджера, щоб не створювати зайвого ажіотажу та черг. Більше в попередньому пості.</p>" },' +
+		'{"title" : "Мешканці 10-ї секції! ", "content": "<p>Ми розпочинаємо процес збору документів на реєстрацію права власності на Вашу секцію. Просимо Вас протягом тижня завітати до центру продажу ЖК Галицький Лев та захопити з собою відповідні документи: договір та паспорт, також не забути кошти на відповідні реєстраційні процедури - 1 400 гривень за один об\'єкт (квартиру, комірку, гараж, підвал тощо).</p><p>Звісно якщо Ви писали заяву на виготовлення технічного паспорту. З метою впорядкування черги, щоб не створювати зайвого ажіотажу в центрі продажів просимо Вас записуватись на подачу документів до менеджерів за телефонами ЦП.  Вам буде призначена зручна для Вас дата та час прийому документів.</p>" },' +
+		'{"title" : "Шановні мешканці! ", "content": "<p>На Ваші чисельні прохання та з метою кращого контролю за якістю та вчасністю розміщення фотозвітів відтепер щотижневе інформування про стан будівництва у секціях, де ведуться роботи, буде здійснюватися у П\'ЯТНИЦЮ, наприкінці робочого дня!</p>" }' +
+		']}';
+		var blog_obj = JSON.parse(blog_data);
+		var blog_index = $(this).closest('.swiper-slide').index();
+		
+		$('.pbox_content .htitle').text(blog_obj.items[blog_index].title);
+		$('.pbox_content .text').html(blog_obj.items[blog_index].content);
+		
 		$('#overlay_blog').fadeIn(200, function(){
 			$('#popup_blog').animate({'right': '0px'}, 200);
 		});
@@ -69,29 +87,6 @@ $(function() {
 		railVisible: true,
 		alwaysVisible: true,
 		railOpacity: 1,
-	});
-	
-	/*news*/
-	$('.news_carousel .pag a.num').click(function(){
-		$(this).closest('.pag').find('a.num').removeClass('active');
-		$(this).addClass('active');
-		return false;
-	});
-	$('.news_carousel .pag a.prev').click(function(){
-		var prev_page = $(this).parent().find('a.num.active').prev('a.num');
-		if(prev_page.length){
-			$(this).closest('.pag').find('a.num').removeClass('active');
-			prev_page.addClass('active');
-		}
-		return false;
-	});
-	$('.news_carousel .pag a.next').click(function(){
-		var next_page = $(this).parent().find('a.num.active').next('a.num');
-		if(next_page.length){
-			$(this).closest('.pag').find('a.num').removeClass('active');
-			next_page.addClass('active');
-		}
-		return false;
 	});
 	
 	/*photo archive*/
@@ -241,7 +236,6 @@ $(function() {
 			 $(this).closest('.flat_detail').find('.btn').removeClass('active');
 		}
 	);
-	
 	$('.area_map area, .b-area-label').hover(
 		function(){
 			 var area_index = $(this).attr('data-section-id');
@@ -259,7 +253,6 @@ $(function() {
 			 $('.b-area-tooltip').hide();
 		}
 	);
-	
 	$('.area_map area').hover(function(){
 		if($(this).attr('data-section-count')!='0'){
 			$('.b-area-tooltip .circle').html('<span class="num">'+$(this).attr('data-section-count')+'</span><br/>квартир');
@@ -267,7 +260,6 @@ $(function() {
 			$('.b-area-tooltip .circle').html('усі<br/> квартири<br/> продані');
 		}
 	});
-	
 	$('.area_map').mousemove(function(e){
 		if(width>720){
 			$('.b-area-tooltip').css({'left': e.pageX-$(this).offset().left-120+'px', 'top': e.pageY-$(this).offset().top+'px'});
@@ -275,6 +267,22 @@ $(function() {
 			$('.b-area-tooltip').css({'left': e.pageX-$(this).offset().left+0+'px', 'top': e.pageY-$(this).offset().top+20+'px'});
 		}
 	});
+	$('#plans13-hovers path, #plans15-hovers path').hover(
+		function(){
+			$(this).css({'opacity': '1'});
+		},
+		function(){
+			$(this).css({'opacity': '0'});
+		}
+	);
+	$('.plans_flats .btn').hover(
+		function(){
+			$('#plans'+$(this).parent().attr('data-flat-id')).css({'opacity': '1'});
+		},
+		function(){
+			$('#plans'+$(this).parent().attr('data-flat-id')).css({'opacity': '0'});
+		}
+	);
 	if(width<720){
 		$('.b-area-hover-13, .b-area-label-13').addClass('active');
 		$('.area_map').attr('data-mob-section-id', '13');
@@ -300,19 +308,35 @@ $(function() {
 		});
 	}
 	
-	/* main banner carousel */	
-	var owl1 = $(".m_banner .owl-carousel");
-	
-	owl1.owlCarousel({
-		items: 1,
-		margin: 0,
-		nav: false,
-		dots: false,
-		loop: true,
-		smartSpeed: 1000,
-		autoplay: true,
-		autoplayTimeout: 5000,
-	});
+	/*news carousel*/
+	var news_swiper = new Swiper('.news_carousel .swiper-container', {
+		//autoplay: 3000,
+		speed: 750,
+		slidesPerView: 3,
+        spaceBetween: 50,
+		slidesPerGroup: 3,
+		nextButton: '.news_carousel .next',
+        prevButton: '.news_carousel .prev',
+		pagination: '.news_carousel .pagination',
+		paginationType: 'bullets',
+		paginationClickable: true,
+		paginationBulletRender: function (index, className) {
+			return '<a class="num ' + className + '">' + (index + 1) + ' <span>сторінка</span></a>';
+		},
+		loop: false,
+		breakpoints: {
+			720: {
+			  slidesPerView: 1,
+			  slidesPerGroup: 1,
+			  spaceBetween: 10
+			},
+			1200: {
+			  slidesPerView: 2,
+			  slidesPerGroup: 2,
+			  spaceBetween: 20
+			}
+		}
+    });
 	
 	/*menu popup*/
 	$('.menu_btn').click(function () {
